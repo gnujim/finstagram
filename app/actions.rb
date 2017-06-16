7fb51df7c244e345b76ae5457d73b0850921ca87
Mijung Lee <mijung.m@gmail.com>
2017-06-16 01:59:07 +0000

    W5D1 - add login and logout page, implement sessions and forms

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @posts = Post.order(created_at: :desc)
   erb(:index)
@@ -9,21 +15,40 @@ get '/signup' do    #if a user navigated to the path "/signup",
 end
 
 post '/signup' do
-  
-  #grab user input values from params
   email       = params[:email]
   avatar_url  = params[:avatar_url]
   username    = params[:username]
   password    = params[:password]
   
-
-  #instantiate and save a User
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
   
-  #if user validations pass and user is saved
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
-    erb( :signup)
+    erb(:signup)
   end
+end
+
+get '/login' do
+  erb( :login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
